d76ec4e4b9ca794f38e17c8984297a9cd7c654f5
Bug-Finderr <sudu.boi.2k5@gmail.com>
2024-04-30 22:28:09 +0530

    #2: Add dtos & admin model

---
+++ src/main/java/com/bugfi/employeemanagementsystem/dtos/AdminDto.java
@@ -0,0 +1,12 @@
+package com.bugfi.employeemanagementsystem.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminDto {
+    private Long id;
+    private String username;
+    private String password;
+}
